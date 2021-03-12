bcb98ba0e4bf81ab052703aad3cb5b5fb826fccc
knksknsy <k.keskinsoy@gmail.com>
2021-03-12 23:48:24 +0100

    Add/Update training script.
    Restart training if "CUDA out of memory" exception is thrown.

--- bash/run_train.sh
+++ bash/run_train.sh
@@ -1,7 +1,33 @@
 #!/bin/sh
-python run.py train --dataset_train /home/kaan/datasets/VoxCeleb2/CropIdsPreprocessed/dev/ \
-                    --dataset_test /home/kaan/datasets/VoxCeleb2/CropIdsPreprocessed/test/ \
-                    --csv_train ./dataset/dataset_crop_ids_dev.csv \
-                    --csv_test ./dataset/dataset_crop_test.csv \
-                    --device cuda \
-                    --config ./configs/config.yaml
+CONFIG="./configs/config.yaml"
+
+CHECKPOINT_DIR="$(cat $CONFIG | grep checkpoint_dir)"
+CHECKPOINT_DIR="${CHECKPOINT_DIR#*": "}" # Remove yaml property; Keep value
+
+DO=1
+while [ $DO -eq 1 ]
+    do
+        # Start training
+        python run.py train --config $CONFIG
+        # Get stdout and stderr
+        RESULT=$?
+
+        # stderr
+        # Catch exception: CUDA out of memory
+        if [ $RESULT -ne 0 ]; then
+            # Get latest checkpoint
+            LATEST_CHECKPOINT="$(ls -t $CHECKPOINT_DIR | head -n 1)"
+            LATEST_CHECKPOINT="${LATEST_CHECKPOINT#"Discriminator_"}"
+            
+            # Add LATEST_CHECKPOINT to config.yaml
+            sed -i "s/continue_id.*/continue_id: $LATEST_CHECKPOINT/" $CONFIG
+
+            echo "Restart training with latest checkpoint_id: $LATEST_CHECKPOINT"
+        
+        # stdout
+        # Exit while loop if training finished without exceptions
+        else
+            DO=0
+            echo "Training finished"
+        fi
+    done
