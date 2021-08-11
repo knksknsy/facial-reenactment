0279bc74ba2478b03e834dcf8101868ba96e9bc0
knksknsy <k.keskinsoy@gmail.com>
2021-08-11 19:12:34 +0200

    #

--- scripts/creation/run_infer_model11.sh
+++ scripts/creation/run_infer_model11.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 python run_creation.py infer \
-                    --source "/Volumes/1\ TB\ Data/facial-reenactment/datasets/VoxCeleb2/CropPreprocessed/test/id00017/5MkXgwdrmJw/1.png" \
-                    --target "/Volumes/1\ TB\ Data/facial-reenactment/datasets/VoxCeleb2/Videos/test/id01567/1Lx_ZqrK1bM/00001.mp4" \
-                    --model "/Volumes/1\ TB\ Data/facial-reenactment/models/creation/exp11/Generator_t20210401_1236_e029_i00150000.pth" \
+                    --source "/media/Beta/facial-reenactment/datasets/VoxCeleb2/CropPreprocessed/test/id00017/5MkXgwdrmJw/1.png" \
+                    --target "/media/Beta/facial-reenactment/datasets/VoxCeleb2/Videos/test/id01567/1Lx_ZqrK1bM/00001.mp4" \
+                    --model "/media/Beta/facial-reenactment/models/creation/exp11/Generator_t20210401_1236_e029_i00150000.pth" \
                     --config ./configs/config_creation_model11.yaml
--- scripts/detection/run_infer.sh
+++ scripts/detection/run_infer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python run_detection.py infer \
-                    --source "/Volumes/1\ TB\ Data/facial-reenactment/datasets/FaceForensics/Videos/test/manipulated_sequences/NeuralTextures/c23/videos/132_007.mp4" \
-                    --model "/Volumes/1\ TB\ Data/facial-reenactment/models/detection/exp6/SiameseResNet_t20210416_2034_e019_i00010710.pth" \
+                    --source "/media/Beta/facial-reenactment/datasets/FaceForensics/Videos/test/manipulated_sequences/NeuralTextures/c23/videos/132_007.mp4" \
+                    --model "/media/Beta/facial-reenactment/models/detection/exp6/SiameseResNet_t20210416_2034_e019_i00010710.pth" \
                     --config ./configs/config_detection_model6.yaml
